2d2ed6f115247c686f87947ee2b7990968b32a81
Andy <1197993367@qq.com>
2017-03-30 22:01:10 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/yousheng/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.yousheng.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yousheng on 17/3/30.
+ */
+//litepal每一个实体类必须继承dataSupport类
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
